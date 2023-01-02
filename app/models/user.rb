881122073590bb908f64423ff912d3147887924b
Baran Yeni <info@baranyeni.com>
2023-01-02 23:19:35 +0300

    JWT to Bearer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  def generate_jwt
-    JWT.encode({ id: id,
-                 exp: 60.days.from_now.to_i },
-               Rails.application.secrets.secret_key_base)
-  end
+  include DeviseTokenAuth::Concerns::User
 end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,1 @@
 json.(user, :id, :email, :birthdate)
-json.token user.generate_jwt
