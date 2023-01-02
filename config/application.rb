23dd97e80add9e4cadf32de39b35952b3cacf69c
Baran Yeni <info@baranyeni.com>
2023-01-02 21:26:29 +0300

    fix `DisabledSessionError`

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module BBBRentacarBe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
+      config.middleware.use ActionDispatch::Cookies
+      config.middleware.use config.session_store, config.session_options
+      config.middleware.use ActionDispatch::Flash
+
   end
 end
