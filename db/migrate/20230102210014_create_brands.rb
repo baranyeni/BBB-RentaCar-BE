59032862f828ff69c58e7d1b98a10cba2b7d1c85
Baran Yeni <info@baranyeni.com>
2023-01-03 00:19:20 +0300

    Create `Brands`

---
+++ db/migrate/20230102210014_create_brands.rb
@@ -0,0 +1,10 @@
+class CreateBrands < ActiveRecord::Migration[7.0]
+  def change
+    create_table :brands do |t|
+      t.string :name, null: false, index: { unique: true }
+      t.string :logo_url
+
+      t.timestamps
+    end
+  end
+end
