11ef67947a04a7d84d43da3d9a12405addab4ccb
Baran Yeni <info@baranyeni.com>
2023-01-04 23:38:04 +0300

    allow DELETE methods fro CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :options], expose: ['Authorization', 'uid', 'client']
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :options, :delete],
+             expose: ['Authorization', 'uid', 'client']
   end
 end
