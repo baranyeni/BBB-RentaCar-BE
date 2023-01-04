441b07e2857da7d036326fce244f9db88113dcd8
Baran Yeni <info@baranyeni.com>
2023-01-04 21:06:43 +0300

    expose headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :options], expose: ['Authorization']
+    resource '*', headers: :any, methods: [:get, :post, :options], expose: ['Authorization', 'uid', 'client']
   end
 end
