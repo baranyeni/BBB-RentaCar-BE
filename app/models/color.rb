fb6c4c12f36a763dce659bcf1f3522c49f56f439
Baran Yeni <info@baranyeni.com>
2023-01-05 06:41:47 +0300

    Color model validations

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,4 @@
 class Color < ApplicationRecord
-  validates :name, uniqueness: true
-  validates :hex, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+  validates :hex, uniqueness: true, presence: true
 end
